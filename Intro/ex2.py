aa3ee68472124b2e80221f0d2cc802857fc610bf
Gadia, Rohit <rohit.gadia@fmr.com>
2017-09-25 20:28:12 +0530

    Gradient Descent Minor update

--- Intro/ex2.py
+++ Intro/ex2.py
@@ -6,18 +6,19 @@ import matplotlib.animation as animation
 
 num_house = 160
 np.random.seed(42)
-house_size = np.random.randint(low=1000,high=3500,size=num_house)
+house_size = np.random.randint(low=1000, high=3500, size=num_house)
 
 np.random.seed(42)
-house_price = house_size * 100 + np.random.randint(low=20000,high=70000,size=num_house)
+house_price = house_size * 100 + np.random.randint(low=20000, high=70000, size=num_house)
 
-plt.plot(house_size,house_price,'bx')
+plt.plot(house_size, house_price, 'bx')
 plt.xlabel("Size")
 plt.ylabel("Price")
 
 plt.show()
 
 #Noramlize the array of data first, this is also referred to as scaling
+
 
 def normalize(array):
     return (array-array.mean())/array.std()
@@ -38,3 +39,31 @@ test_house_price_norm = normalize(test_house_price)
 
 tf_house_size = tf.placeholder("float",name="house_size")
 tf_house_price = tf.placeholder("float",name="house_price")
+
+tf_size_factor = tf.Variable(np.random.randn(),name="size_factor")
+tf_price_offset = tf.Variable(np.random.randn(),name="price_offset")
+
+#Now that our data is ready, we can focus on the prediction model.
+#Here's a simple model that predicts the prices based on the house size and two other variables i.e. size_factor and price_offset
+tf_price_pred = tf.add(tf.multiply(tf_size_factor,tf_house_size),tf_price_offset)
+
+#After defining the modeel our aim is to reduce the cost so that our model finds a best fit in to the data and predicts accurately.
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred-tf_house_price,2))/(2*num_train_samples)
+
+#Now we define a learning rate for our model which depends on Gradient descent to find the best fit in to the data.
+learning_rate = 0.1
+
+#Now finally use tensorflow's gradient descent optimizer to minimize the cost of the predictor
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    display_every = 2
+    num_training_iter = 50
+
+    for iteration in range(num_training_iter):
+        for (x,y) in zip(train_house_size_norm,train_house_price_norm):
+            sess.run(optimizer)
